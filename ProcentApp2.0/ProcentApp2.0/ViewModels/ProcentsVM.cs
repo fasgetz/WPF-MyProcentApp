8cc740c8aad2837d9502c7aebdf2912c3a63ed5d
Andrew Andrew <fasgetz@hotmail.com>
2019-07-17 21:00:04 +0300

    End work

--- ProcentApp2.0/ProcentApp2.0/ViewModels/ProcentsVM.cs
+++ ProcentApp2.0/ProcentApp2.0/ViewModels/ProcentsVM.cs
@@ -1,4 +1,5 @@
-﻿using ProcentApp2._0.Models;
+﻿using ProcentApp2._0.AppLogic;
+using ProcentApp2._0.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,6 +13,30 @@ namespace ProcentApp2._0.ViewModels
     class ProcentsVM : INotifyPropertyChanged
     {
         #region Свойства
+
+        private double _EarnedSum; // Заработанная сумма
+        private double _AllCapital; // Вся итоговая сумма (Стартовый капитал + Заработанная сумма)
+
+        public double EarnedSum
+        {
+            get => _EarnedSum;
+            set
+            {
+                _EarnedSum = value;
+                OnPropertyChanged("EarnedSum");
+            }
+        }
+
+        public double AllCapital
+        {
+            get => _AllCapital;
+            set
+            {
+                _AllCapital = value;
+                OnPropertyChanged("AllCapital");
+            }
+        }
+
 
         // Месяцев в обороте
         private int _Month;
@@ -53,12 +78,65 @@ namespace ProcentApp2._0.ViewModels
         {
             mymodel = model;
 
+            list = new List<string>();
+
+
             // Получаем сколько всего месяцев капитал будет в обороте
             Month = (mymodel.EndDate - DateTime.Now).Days / 30; // Количество месяцев
+
+            AllCapital = mymodel.StartSum; // Для инициализации общего капитала
+            EarnedSum = 0; // Заработанная сумма
+
+
+            StartInvseting();
+            AllCapital += EarnedSum;
+
         }
 
         #region Методы
 
+
+        // Стартовый метод для инициализации
+        private void StartInvseting()
+        {
+            double residue = 0; // Остаточная сумма
+            double startcap = mymodel.StartSum; // Стартовый капитал
+
+            // Считаем заработок за каждый месяц
+            for (int i = 0; i < Month; i++)
+            {
+                double MonthSum = Calculate.GetProcent(startcap, mymodel.Procent); // Месячный доход
+
+                // Добавляем к остаточной сумме нашу зп (если ее выбрали)
+                if (mymodel.PutOff != 0)
+                    MonthSum += mymodel.PutOff;
+
+                residue += MonthSum; // Добавляем к остаточной сумме 
+
+                EarnedSum += MonthSum; // Прибавляем к заработанному капиталу сумму
+
+
+                string s = $"Месяц {i + 1} | Доход с процентов {MonthSum - mymodel.PutOff}";
+                if (mymodel.PutOff != 0)
+                    s += $" | Сумма всего (С зп): {MonthSum}";
+
+                list.Add(s);
+
+
+
+                // Если заработанная сумма достигла больше суммы, которую можно добавить,
+                // то добавь к капиталу для увеличения ежемесечных процентов
+                if (residue > mymodel.Summarize && mymodel.Summarize != 0)
+                {
+
+                    var temp = residue - mymodel.Summarize; // Остаточная сумма
+                    startcap += mymodel.Summarize; // Добавляем к капиталу накопленную сумму. Теперь будет больше %
+
+                    residue = temp; // Остаточная сумма = Накопленная сумма - добавляемая сумма
+
+                }
+            }
+        }
 
 
         #endregion
